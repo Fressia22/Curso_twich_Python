86f78c5c0373dba780a70253544f5a56e502d025
Fressia22 <fabrina.rovaretti@gmail.com>
2023-02-03 14:46:48 -0300

    culminada la clase 11

--- 11_Classes.py
+++ 11_Classes.py
@@ -14,14 +14,20 @@ print(MyEmptyPerson())
 
 class Person:
     # Definimos un contructor de clase con __init__:
-    def __init__(self, name, surname, dni = "indocumentadx"):
+    def __init__(self, name, surname, alias = "Sin alias"):
         #le asignamos a self una propiedad name, y le asignamos un valor
-        self.full_name = f"{name} {surname} DNI N°: {dni}"
+        self.full_name = f"{name} {surname} ({alias})"
        
     def walk (self):
         print(f"{self.full_name} está caminando")
 
-my_person = Person("Fabri", "Rovaretti", 33270596)
+my_person = Person("Fabri", "Rovaretti", "Flakira")
 print(my_person.full_name)
 my_person.walk()
 
+my_other_person = Person("Daniel", "Rovaretti", "Chapulin")
+print(my_other_person.full_name)
+my_other_person.walk()
+
+my_other_person.full_name = "Franco Rovaretti (Rova)"
+print(my_other_person.full_name)
